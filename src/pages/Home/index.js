d2f928ede7b1358ebf88246ca902e4bfa8694a16
Your Name <you@example.com>
2022-07-01 08:59:32 +0800

    Index Edit update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,13 @@
+import Edit from './components/Edit'
+import List from './components/List'
+import './index.css'
+
+
 const Home = () => {
-    return <div>Home</div>
+    return <div className="app">
+        <Edit />
+        <List />
+    </div>
 }
 
 export default Home
