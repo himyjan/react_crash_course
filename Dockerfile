f43b6ed838baa05eb73c3040af66fac1c59c9ba1
Your Name <you@example.com>
2022-07-15 03:43:48 +0800

    update package version

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,17 @@
-#docker build -t react . --no-cache && docker run react
 # Stage 1 - Build React App inside temporary Node container
-# pull official base image
 FROM node:alpine as react-build
 
-# set working directory
-WORKDIR /app
-
-# add `/app/node_modules/.bin` to $PATH
-ENV PATH /app/node_modules/.bin:$PATH
-
-# install app dependencies
-COPY package.json ./
-COPY package-lock.json ./
-RUN npm install
-
-# add app
+WORKDIR /usr/src/app
 COPY . ./
+RUN npm install
+RUN npm run-script build
 
-# start app
-CMD ["npm", "start"]
+# Stage 2 - Deploy with NGNIX
+FROM nginx:latest
 
-# WORKDIR /usr/src/app
-# COPY . ./
-# RUN npm install
-# RUN npm run start
+COPY --from=react-build /usr/src/app/build /var/www
+COPY nginx.conf /etc/nginx/nginx.conf
 
-# # Stage 2 - Deploy with NGNIX
-# FROM nginx:latest
+EXPOSE 3000
 
-# COPY --from=react-build /usr/src/app/build /var/www
-# COPY nginx.conf /etc/nginx/nginx.conf
-
-# EXPOSE 3000
-
-# ENTRYPOINT ["nginx","-g","daemon off;"]
+ENTRYPOINT ["nginx","-g","daemon off;"]
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 
-import Edit from './components/Edit'
-import List from './components/List'
-import './index.css'
-
+import Edit from "./components/Edit";
+import List from "./components/List";
+import "./index.css";
 
 const Home = () => {
-    const [a, setA] = useState(100)
-    const [data, setData] = useState([])
+  const [a, setA] = useState(100);
+  const [data, setData] = useState([]);
 
-    function plus() {
-        setA(function (prev) {
-            return prev + 200
-        })
-    }
+  useEffect(() => {
+    // 綁定的事情
+    return () => {
+      // 取消綁定
+    };
+    window.alert("新增成功");
+  }, [data]);
 
-    return <div className="app">
-        {a}
-        <button onClick={plus}>加法</button>
-        <Edit add={setData} />
-        <List listData={data} deleteData={setData}/>
+  function plus() {
+    setA(function (prev) {
+      return prev + 200;
+    });
+  }
+
+  return (
+    <div className="app">
+      {a}
+      <button onClick={plus}>加法</button>
+      <Edit add={setData} />
+      <List listData={data} deleteData={setData} />
     </div>
-}
+  );
+};
 
-export default Home
+export default Home;
